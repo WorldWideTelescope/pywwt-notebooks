5cd4866e0e7570cda1e134258902fc364ff00689
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-05-20 12:34:39 +0100

    Enable Jupyter Lab

--- binder/setup.sh
+++ binder/setup.sh
@@ -5,4 +5,12 @@ set -ex
 git config --global user.email "binder@user.com"
 git config --global user.name "Binder"
 
+# Set up Jupyter Lab
+pip install jupyterlab --user
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+
+# Install main packages needed for notebook
 pip install "pywwt>=0.5.2" PyQt5 "astroquery>=0.3.9" "PyYAML>=3.1.3" --user
+
+# Re-build Jupyter Lab
+jupyter lab build
