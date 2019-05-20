09c9351f25e81c83cc73fe620e2d53adb5d44aca
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-05-20 12:12:27 +0100

    Fixed permissions for setup.sh, and make sure we don't redirect output in pip command.

--- binder/setup.sh
+++ binder/setup.sh
@@ -5,4 +5,4 @@ set -ex
 git config --global user.email "binder@user.com"
 git config --global user.name "Binder"
 
-pip install pywwt>=0.5.2 PyQt5 astroquery>=0.3.9 PyYAML>=3.1.3
+pip install "pywwt>=0.5.2" PyQt5 "astroquery>=0.3.9" "PyYAML>=3.1.3" --user
