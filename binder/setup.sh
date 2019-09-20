0370f24df44b86cfcf33f2965c3018f24d64c029
Peter Williams <peter@newton.cx>
2019-09-20 15:31:59 -0400

    binder/setup.sh: trigger update to newer pywwt

--- binder/setup.sh
+++ binder/setup.sh
@@ -10,7 +10,7 @@ pip install jupyterlab --user
 jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
 
 # Install main packages needed for notebook
-pip install "pywwt>=0.6.1" PyQt5 "astroquery>=0.3.9" "PyYAML>=3.1.3" --user
+pip install "pywwt>=0.7.0" PyQt5 "astroquery>=0.3.9" "PyYAML>=3.1.3" --user
 
 # Re-build Jupyter Lab
 jupyter lab build
