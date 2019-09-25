98b1ef47cff7d3443661caaed543d59f03d75477
Peter Williams <peter@newton.cx>
2019-09-24 21:58:20 -0400

    binder/setup.sh: make sure to install ipyevents as a lab extension

--- binder/setup.sh
+++ binder/setup.sh
@@ -5,9 +5,9 @@ set -ex
 git config --global user.email "binder@user.com"
 git config --global user.name "Binder"
 
-# Set up Jupyter Lab
-pip install jupyterlab --user
-jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+# Set up Jupyter Lab and needed extensions
+pip install jupyterlab ipyevents --user
+jupyter labextension install @jupyter-widgets/jupyterlab-manager ipyevents --no-build
 
 # Install main packages needed for notebook
 pip install "pywwt>=0.7.0" PyQt5 "astroquery>=0.3.9" "PyYAML>=3.1.3" --user
