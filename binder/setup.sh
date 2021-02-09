c937a99b84b2084cc1fa2f4787fd3002805291df
Peter Williams <peter@newton.cx>
2021-02-09 13:35:09 -0500

    binder/setup.sh: update to use the app-powered releases!

--- binder/setup.sh
+++ binder/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-set -ex
+set -xeuo pipefail
 
 git config --global user.email "binder@user.com"
 git config --global user.name "Binder"
 
 # Set up Jupyter Lab and needed extensions
 pip install jupyterlab ipyevents --user
-jupyter labextension install @jupyter-widgets/jupyterlab-manager ipyevents --no-build
+jupyter labextension install  --no-build \
+  @jupyter-widgets/jupyterlab-manager \
+  ipyevents \
+  @wwtelescope/jupyterlab
 
 # Other necessary/useful packages
 pip install PyQt5 "astroquery>=0.3.9" "PyYAML>=3.1.3" --user
@@ -15,7 +18,7 @@ pip install PyQt5 "astroquery>=0.3.9" "PyYAML>=3.1.3" --user
 # Finally, pywwt. BinderHub only knows to rebuild its images when this file or
 # the Dockerfile changes, so we need a scheme that gives us a nice reason to
 # update this file when we want to target a different version of pywwt.
-pip install https://github.com/WorldWideTelescope/pywwt/archive/pypa/pywwt@0.10.4.zip --user
+pip install https://github.com/WorldWideTelescope/pywwt/archive/pypa/pywwt@0.11.0.zip --user
 
 # Re-build Jupyter Lab with debuggability for mybinder.org
 # 2020 Oct: setting --minimize=False to try to get mybinder building
