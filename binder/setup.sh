e71fb5ab7a9f413cd249e922908ea14d3d3882f5
Peter Williams <peter@newton.cx>
2021-10-29 15:24:45 -0400

    binder/setup.sh: demo system-wide configuration of bundled app

--- binder/setup.sh
+++ binder/setup.sh
@@ -25,6 +25,20 @@ pip install \
 # update this file when we want to target a different version of pywwt.
 pip install https://github.com/WorldWideTelescope/pywwt/archive/pypa/pywwt@0.15.1.zip --user
 
+# Demonstrate how to configure the JupyterLab install to use the bundled app
+# instead of the hosted version. This isn't actually necessary with MyBinder,
+# but it's good to test that the functionality works.
+
+settings=$(jupyter --data-dir)/lab/settings
+mkdir -p "$settings"
+cat <<'EOF' >"$settings/overrides.json"
+{
+  "@wwtelescope/jupyterlab:research": {
+    "appUrl": "/wwtstatic/research/"
+  }
+}
+EOF
+
 # Re-build Jupyter Lab with debuggability for mybinder.org
 # 2020 Oct: setting --minimize=False to try to get mybinder building
 log=$(mktemp)
